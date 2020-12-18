8a277f5edd8b6e42dfe633fb9078ddc88ce34df6
Leonardo Martins de Sousa <leonardomds2013@gmail.com>
2020-12-18 12:04:09 -0300

    Create aVeryBigSum

---
+++ facil/a-very-big-sum.js
@@ -0,0 +1,15 @@
+/* 
+  Dado uma lista de inteiros retorne a some de todos os elementos, lembrando que alguns desses números podem ser muito grandes.
+  exemplo de input: [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+  resultado esperado: 5000000015
+*/
+
+function aVeryBigSum(lista) {
+  let somaDosElementos = 0;
+  
+  lista.forEach( (elemento) => {
+    somaDosElementos += elementos;
+  });
+  
+  return somaDosElementos;
+};
