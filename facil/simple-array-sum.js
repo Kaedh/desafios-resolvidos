e993c3e86e800c1969bd831a40c81ab0e2d1d557
Leonardo Martins de Sousa <leonardomds2013@gmail.com>
2020-12-18 11:52:48 -0300

    organizando repositório em  pastas.

---
+++ facil/simple-array-sum.js
@@ -0,0 +1,17 @@
+/*
+  Dada uma lista de de inteiros, encontre a soma de todos os seus elementos.
+  
+  exemplo de input: [ 1, 2, 3 ]
+  resultado esperado: 6
+*/
+
+
+function simpleArraySum(lista) {
+  let elementosSomados = 0;
+  
+  lista.forEach( (elemento) => {
+    elementosSomados += elemento;
+  });
+  
+  return elementosSomados;
+};
